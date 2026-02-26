186bac12996a7d12461fe42f8ad32859e0530f24
angrypig555 <brny@brny.dev>
2026-02-26 19:30:34 +0000

    make it work

--- src/main.cpp
+++ src/main.cpp
@@ -1,5 +1,6 @@
 #include<iostream>
 #include<sstream>
+#include<string>
 
 #include<curlpp/cURLpp.hpp>
 #include<curlpp/Easy.hpp>
@@ -8,12 +9,14 @@
 
 #include<nlohmann/json.hpp>
 
-int main() {
+int full(char *word, int is_simple) {
     long response_code;
     std::stringstream stream;
+    std::string request_url;
     try {
         curlpp::Easy request;
-        request.setOpt<curlpp::options::Url>("https://api.dictionaryapi.dev/api/v2/entries/en/hello");
+        request_url = "https://api.dictionaryapi.dev/api/v2/entries/en/" + std::string(word);
+        request.setOpt<curlpp::options::Url>(request_url);
         request.setOpt(new curlpp::options::WriteStream(&stream));
         request.perform();
 
@@ -26,7 +29,7 @@ int main() {
         std::cerr << e.what() << std::endl;
     }
     if (response_code != 200) {
-        std::cerr << "failed to contact api with response " << response_code << std::endl;
+        std::cerr << "failed to contact api with response " << response_code << " requested url: " << request_url << std::endl;
         return 1;
     } else {
         nlohmann::json data = nlohmann::json::parse(stream);
@@ -35,16 +38,47 @@ int main() {
             auto first_result = data[0];
 
             for (const auto& meaning : first_result["meanings"]) {
-                std::cout << "word type: " << meaning["partOfSpeech"] << std::endl;
+                if (is_simple == 0) {
+                    std::cout << "word type: " << meaning["partOfSpeech"] << std::endl;
+                }
                 for (const auto& definitions : meaning["definitions"]) {
                     std::string def = definitions["definition"];
-                    std::cout << "definition: " << def << std::endl;
+                    if (is_simple == 0) {
+                        std::cout << "definition: " << def << std::endl;
+                    } else {
+                        std::cout << def << std::endl;
+                        return 0;
+                    }
                 }
             }
         }
         catch (nlohmann::json::exception & e) {
             std::cerr << "parse error " << e.what() << " raw dump of response " << stream.str() << std::endl;
         }
+    }
+    return 0;
+}
+
+int simple(char *word) {
+    return 0;
+}
+
+int main(int argc, char *argv[]) {
+    if (argc < 2) {
+        std::cerr << "no arguments passed, see --help for more information" << std::endl;
+        return 1;
+    }
+    std::string argument_one = argv[1];
+    if (argument_one == "--help") {
+        std::cout << "define - a terminal tool to look up definitions of words" << std::endl;
+        std::cout << "--help - display help menu" << std::endl;
+        std::cout << "--full - display full definition and word types of a word" << std::endl;
+        std::cout << "if no other arguments are passed, it will display the definition in simple mode" << std::endl;
+        std::cout << "usage: define [OPTION] [WORD]" << std::endl;
+    } else if (argument_one == "--full") {
+        full(argv[2], 0);
+    } else {
+        full(argv[1], 1);
     }
     return 0;
 }
